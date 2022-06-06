4d2a9dab1553bc86cdd9a3f835b2f6754f2185a2
Eryk Bojaruniec <erykb412@wp.pl>
2022-06-06 22:10:56 +0200

    Przywrocono dobra wersje, kod zawiera klasy Word i Game z konstruktorami i polami, jakies gettery, w klasie Game pierwsze metody obl=slugujace logike

--- src/com/Project/Game.java
+++ src/com/Project/Game.java
@@ -1,2 +1,41 @@
-package com.Project;public class Game {
+package com.Project;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game extends Word {
+    private int lives;
+    private List<String> used_Characters = new ArrayList();
+
+    public Game(int lives, int length, String content) {
+        super(length, content);
+        this.lives = lives;
+    }
+
+    public void PrintTheCurrentStateOfWord(String content){
+            String[] the_Word_Character_By_Character = content.split("");
+            for(String letter : the_Word_Character_By_Character){
+                if(this.used_Characters.contains(letter)) {
+                    System.out.print(letter);
+                }
+                else {
+                    System.out.print("_");
+                }
+        }
+    }
+
+    public boolean AnalyzeTheLetter(String letter){
+        boolean flak = true;
+        if(letter.length() != 1){
+            System.out.println("You need to enter exactly one english alphabet letter. Try again");
+            flak = false;
+        }
+        else if(Character.isLetter(letter.charAt(0)) == false){
+            System.out.println("You need to enter an english alphabet letter. Try again");
+            flak = false;
+        }
+        return flak;
+    }
+
 }
--- src/com/Project/Word.java
+++ src/com/Project/Word.java
@@ -1,2 +1,17 @@
 package com.Project;public class Word {
+    private int length;
+    private String content;
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Word(int length, String content) {
+        this.length = length;
+        this.content = content;
+    }
 }
