b78652a024f9ea45d2f9b5b06cb5efb046c85df1
Eryk Bojaruniec <erykb412@wp.pl>
2022-06-07 17:10:15 +0200

    Okazalo sie ze main wyparowal, przywrocono jego stan sprzed znikniecia

--- src/com/Project/Main.java
+++ src/com/Project/Main.java
@@ -1,12 +1,20 @@
 package com.Project;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        List<String> hangman_Words = Arrays.asList("brbst", "gwegtrwg");
+        Random random = new Random();
+        String random_word = hangman_Words.get(random.nextInt(hangman_Words.size()));
+
+        Game game = new Game(5, random_word.length(), random_word);
+
+        game.PrintTheCurrentStateOfWord(random_word);
+        String letter = scanner.nextLine();
+        game.AnalyzeTheLetter(letter);
     }
 }
